3b41e670f15ec6b6d1bc50edf5a3cc1dbb6104c7
Ajay Kumar Santra <ajaykumarsantra@live.com>
2017-08-11 20:31:00 +0530

    password hash.

--- routes/UserAuth.js
+++ routes/UserAuth.js
@@ -1,8 +1,72 @@
 'use strict';
 
+var passwordHash = require('password-hash');
+
 /*
- * GET customers listing.
+ * Check User Credentials.
  */
+exports.login = function (req, res) {
+    if (req.body.UserId == null || req.body.Password == null) {
+        res.send({
+            ResponseCode: 400,
+            ErrorMessage: 'Please send the value in {"UserId":"","Password":""} format.',
+            Data: []
+        });
+        return;
+    }
+    var _userID = req.body.UserId;
+    var _password = req.body.Password;
+
+    req.getConnection(function (err, connection) {
+        let sql = 'CALL UserAuthentication(?)';
+        connection.query(sql, [_userID], (error, results, fields) => {
+            if (error) {
+                res.send({
+                    "ResponseCode": 400,
+                    "ErrorMessage": "Unknown error ocurred."
+                });
+            } else {
+                if (results.length > 0) {
+                    if (results[0][0].Message.indexOf('Invalid') <= -1) {
+                        if (passwordHash.verify(_password, results[0][0].Password)) {
+                            res.send({
+                                "ResponseCode": 200,
+                                "ErrorMessage": "",
+                                "Data": results[0]
+                            });
+                        }
+                        else {
+                            res.send({
+                                "ResponseCode": 204,
+                                "ErrorMessage": "Password not match.",
+                                "Data": []
+                            });
+                        }
+                    }
+                    else {
+                        res.send({
+                            "ResponseCode": 204,
+                            "ErrorMessage": results[0][0].Message,
+                            "Data": []
+                        });
+                    }
+                }
+                else {
+                    res.send({
+                        "ResponseCode": 204,
+                        "ErrorMessage": "User Id does not exits",
+                        "Data": []
+                    });
+                }
+            }
+        });
+    });
+}
+
+
+/*
+* User Registration (Through email and social network) 
+*/
 exports.register = function (req, res) {
     // console.log("req",req.body);
     var today = new Date();
@@ -32,44 +96,3 @@ exports.register = function (req, res) {
 }
 
 
-exports.login = function (req, res) {
-    var userID = req.body.UserId;
-    var password = req.body.Password;
-    req.getConnection(function (err, connection) {
-        connection.query('SELECT * FROM UserAuthentication WHERE UserID = ?', [userID], function (error, results, fields) {
-            if (error) {
-                // console.log("error ocurred",error);
-                res.send({
-                    "ResponseCode": 400,
-                    "failed": "error ocurred"
-                })
-            } else {
-                // console.log('The solution is: ', results);
-                if (results.length > 0) {
-                    if (results[0].Password == password) {
-                        res.send({
-                            "ResponseCode": 200,
-                            "success": "login sucessfull",
-                            Data : results
-                        });
-                    }
-                    else {
-                        res.send({
-                            "ResponseCode": 204,
-                            "success": "User Id and password does not match"
-                        });
-                    }
-                }
-                else {
-                    res.send({
-                        "ResponseCode": 204,
-                        "success": "User Id does not exits"
-                    });
-                }
-            }
-        });
-    });
-}
-
-
-// https://medium.com/technoetics/handling-user-login-and-registration-using-nodejs-and-mysql-81b146e37419
